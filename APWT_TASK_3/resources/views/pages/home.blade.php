fd8f4047f05a763df2a74ef1a6e722968da47260
shOumik-saha <shoumiksahapranto@gmail.com>
2022-06-28 21:36:35 +0600

    lab3

---
+++ APWT_TASK_3/resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+
+@extends('layouts.app')
+@section('content')
+<center>
+    <h1>Welcome to home page</h1>
+</center>
+@endsection
---
+++ APWT_TASK_3/resources/views/user/homeUser.blade.php
@@ -0,0 +1,5 @@
+
+@extends('layouts.appUsers')
+@section('contentUsers')
+    <h1>Welcome to the User Home</h1>
+@endsection
