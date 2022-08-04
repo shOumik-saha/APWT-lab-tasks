0f6840f6350bdf6013d8b87cdf5b7dabfd99c528
shOumik-saha <shoumiksahapranto@gmail.com>
2022-08-05 00:44:39 +0600

    lab8 incomplete

---
+++ apwt_task_8/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Badge } from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <div >
+        <h1> APWT_TASK_8 </h1> 
+        </div>
+    )
+}
+
+export default Home;
